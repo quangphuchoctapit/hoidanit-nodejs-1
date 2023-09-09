1ff571735c69e63a0fe33973c49819515c032866
Fuc le <quangphuchoctapit@gmail.com>
2023-09-09 20:05:16 +0700

    design navbar

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,15 +1,7 @@
 const connection = require('../config/database')
 
 const getHomepage = (req, res) => {
-    let users = []
-    connection.query(
-        'select * from Users u',
-        function (err, results, fields) {
-            console.log('>>>results=', results)
-            users = results
-            res.send(JSON.stringify(users))
-        },
-    )
+    return res.render('home.ejs')
 }
 const getSample = (req, res) => {
     res.render('sample.ejs')
