918d8591aa6aa1ca29bda71c5fc0db8f8491b946
Fuc le <quangphuchoctapit@gmail.com>
2023-09-10 12:56:15 +0700

    create user data

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -8,8 +8,22 @@ const getSample = (req, res) => {
 }
 
 const postCreateUser = (req, res) => {
-    console.log(req.body)
-    res.send('craete new user')
+    let email = req.body.email
+    let name = req.body.myname
+    let city = req.body.city
+    console.log(email, '|', name, '|', city)
+    //     insert into Users (email, name, city)
+    // values ('test', 'eric and hoidanit', 'nam dinh')
+
+    connection.query(
+        `insert into Users(email, name,city)
+        values (?, ?, ?)`, [email, name, city],
+        function (err, results) {
+            console.log(results)
+            res.send('create use succeeded')
+        }
+    )
+
 }
 
 module.exports = {
