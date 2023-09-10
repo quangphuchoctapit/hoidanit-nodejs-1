fb1bf1d6027af31e8d158dcbb202a7306352017a
Fuc le <quangphuchoctapit@gmail.com>
2023-09-10 20:46:28 +0700

    query list users

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,10 @@
 const connection = require('../config/database')
+const { getAllUsers } = require('../services/CRUDService')
 
-const getHomepage = (req, res) => {
-    return res.render('home.ejs')
+const getHomepage = async (req, res) => {
+    let results = await getAllUsers()
+    console.log('check results', results)
+    return res.render('home.ejs', { listUsers: results })
 }
 const getSample = (req, res) => {
     res.render('sample.ejs')
